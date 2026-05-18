7b822ba9f9a6192b8235c48d1bd041f10bf67ccc
ivannissimrch <ivannissimrch@gmail.com>
2026-05-18 19:48:21 -0400

    replace page-specific CSS variable with generic --entity-table-gap in EntityTableList

--- src/components/Dashboard/common/EntityTableList/styles.ts
+++ src/components/Dashboard/common/EntityTableList/styles.ts
@@ -3,6 +3,6 @@ import styled from "styled-components";
 export const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
-  gap: var(--opportunities-container-gap);
+  gap: var(--entity-table-gap);
   width: 100%;
 `;
