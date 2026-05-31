ea8ff0bbc1053a3f1999f27703dd182a2324fe09
ivannissimrch <ivannissimrch@gmail.com>
2026-05-31 14:19:38 -0400

    add horizontal scroll to dashboard table list

--- src/components/Dashboard/common/EntityTableList/styles.ts
+++ src/components/Dashboard/common/EntityTableList/styles.ts
@@ -1,8 +1,18 @@
 import styled from "styled-components";
+import { Table, TableContainer } from "@/components/core/common/Table";
 
 export const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
   gap: var(--entity-table-gap);
   width: 100%;
+`;
+
+export const ScrollableTableContainer = styled(TableContainer)`
+  overflow-x: auto;
+`;
+
+export const ScrollableTable = styled(Table)`
+  width: max-content;
+  min-width: 100%;
 `;
