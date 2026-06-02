09b0286f99c482e1098d6c9f7a1c68497167482e
ivannissimrch <ivannissimrch@gmail.com>
2026-06-02 07:53:32 -0400

    fix: use min-width on table wrapper to prevent window overflow when filter is open

--- src/components/Dashboard/common/EntityTableList/styles.ts
+++ src/components/Dashboard/common/EntityTableList/styles.ts
@@ -6,7 +6,7 @@ export const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
   gap: var(--entity-table-gap);
-  width: 100%;
+  min-width: 85%;
 `;
 
 export const ScrollableTableContainer = styled(TableContainer)`
