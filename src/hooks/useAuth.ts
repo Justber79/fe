03669d75bf48d8d425d8b2d1597a205c26ca0c0e
DarrellRoberts <mrdarrellroberts@gmail.com>
2026-06-13 23:34:52 +0200

    adds auth view for opp profile

---
+++ src/hooks/useAuth.ts
@@ -0,0 +1,24 @@
+import { Id, UserRole } from "need4deed-sdk";
+import { useCurrentUser } from "./useCurrentUser";
+import { useEffect, useState } from "react";
+
+export const useAuth = (contactPersonId?: Id) => {
+  const [isAuthorized, setIsAuthorized] = useState<boolean>(false);
+  const [isOwnProfile, setIsOwnProfile] = useState<boolean>(false);
+
+  const user = useCurrentUser();
+
+  useEffect(() => {
+    const userIsAuthorized = user?.role === UserRole.ADMIN || user?.role === UserRole.COORDINATOR;
+    const personId = user?.personId;
+    if (userIsAuthorized) setIsAuthorized(true);
+    if (contactPersonId && personId === contactPersonId) {
+      setIsOwnProfile(true);
+    } else {
+      setIsOwnProfile(false);
+    }
+    return () => setIsAuthorized(false);
+  }, [user, contactPersonId]);
+
+  return { isAuthorized, isOwnProfile };
+};
