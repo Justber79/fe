fb28a8c0feb1775b007af265884474c498793f41
DarrellRoberts <mrdarrellroberts@gmail.com>
2026-06-15 16:53:22 +0200

    adds serverside user/me fetch & role check on /agent/[id] route

--- src/app/[lang]/dashboard/agents/[id]/page.tsx
+++ src/app/[lang]/dashboard/agents/[id]/page.tsx
@@ -1,7 +1,19 @@
 import ProfileLayout from "@/components/Dashboard/Profile/ProfileLayout";
+import { getServerUserRole } from "@/hooks/api/getUserRole";
 import { RouteParams } from "@/types";
+import { UserRole } from "need4deed-sdk";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
 
 export default async function DashboardAgentPage({ params }: RouteParams) {
   const { id } = await params;
+
+  const cookieStore = await cookies();
+  const cookieHeader = cookieStore.toString();
+  const userRole = await getServerUserRole(cookieHeader);
+
+  if (!userRole || (userRole !== UserRole.COORDINATOR && userRole !== UserRole.ADMIN)) {
+    redirect(`/dashboard/agents`);
+  }
   return <ProfileLayout entityId={id} entityType="agent" />;
 }
