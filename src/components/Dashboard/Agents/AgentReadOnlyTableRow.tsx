5b542488a6dc72619a3c0d5ae664f40fd828439c
DarrellRoberts <mrdarrellroberts@gmail.com>
2026-06-17 11:50:47 +0200

    removes click handler

--- src/components/Dashboard/Agents/AgentReadOnlyTableRow.tsx
+++ src/components/Dashboard/Agents/AgentReadOnlyTableRow.tsx
@@ -1,7 +1,5 @@
 import { ApiAgentGetList, OptionItem } from "need4deed-sdk";
 import { ClickableRow, TableCell } from "@/components/core/common/Table";
-import { useTranslation } from "react-i18next";
-import { useRouter } from "next/navigation";
 
 interface Props {
   agent: ApiAgentGetList;
@@ -12,17 +10,11 @@ interface Props {
 }
 
 export function AgentReadOnlyTableRow({ agent, isLast, districtsList }: Props) {
-  const { i18n } = useTranslation();
-  const router = useRouter();
   const { id, title, type, district } = agent;
   const districtTitle = district?.id ? (districtsList?.find((d) => d.id === district.id)?.title ?? null) : null;
 
-  const handleGoToProfile = () => {
-    if (!id) return;
-    router.push(`/${i18n.language}/dashboard/agents/${id}`);
-  };
   return (
-    <ClickableRow $isLast={isLast} $cursor={"auto"} data-testid={`agent-row-${id}`} onClick={handleGoToProfile}>
+    <ClickableRow $isLast={isLast} $cursor={"auto"} data-testid={`agent-row-${id}`}>
       <TableCell data-testid={`agent-title-${id}`}>{title}</TableCell>
       <TableCell data-testid={`agent-type-${id}`}>{type}</TableCell>
       <TableCell data-testid={`agent-district-${id}`}>{districtTitle || "—"}</TableCell>
