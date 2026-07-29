968e3ecccbeaf979539ce29e34fc96b1d9647629
DarrellRoberts <mrdarrellroberts@gmail.com>
2026-07-29 22:51:57 +0200

    removes useState & useEffect from hook

--- src/hooks/useGetVolunteerOpportunityLinked.ts
+++ src/hooks/useGetVolunteerOpportunityLinked.ts
@@ -1,11 +1,8 @@
 import { ApiVolunteerOpportunityGet, Id } from "need4deed-sdk";
 import { useGetQuery } from "./useGetQuery";
 import { apiPathOpportunity, cacheTTL } from "@/config/constants";
-import { useEffect, useState } from "react";
 
 export const useGetVolunteerOpportunityLinked = (opportunityId: Id, volunteerId?: string) => {
-  const [isAlreadyMatched, setIsAlreadyMatch] = useState<boolean>(false);
-
   const { data, isLoading } = useGetQuery<ApiVolunteerOpportunityGet[]>({
     queryKey: ["opportunity-volunteers", String(opportunityId)],
     apiPath: `${apiPathOpportunity}/${opportunityId}/volunteer-linked`,
@@ -13,14 +10,8 @@ export const useGetVolunteerOpportunityLinked = (opportunityId: Id, volunteerId?
     enabled: !!opportunityId,
   });
 
-  useEffect(() => {
-    if (!volunteerId || isLoading) return;
-    const volunteerIds = data?.map((vol) => vol.volunteerId);
-    if (volunteerIds?.includes(Number(volunteerId))) {
-      setIsAlreadyMatch(true);
-    }
-    return () => setIsAlreadyMatch(false);
-  }, [data, isLoading, volunteerId, opportunityId]);
+  const isAlreadyMatched =
+    !isLoading && !!volunteerId && (data ?? []).some((vol) => vol.volunteerId === Number(volunteerId));
 
   return { data, isLoading, isAlreadyMatched };
 };
