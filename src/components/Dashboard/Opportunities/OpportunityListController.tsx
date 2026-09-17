6833bf2d315aadde19ae26536305e4e1228a1039
DarrellRoberts <mrdarrellroberts@gmail.com>
2026-09-17 16:08:25 +0200

    fixes num of opps w/ view change

--- src/components/Dashboard/Opportunities/OpportunityListController.tsx
+++ src/components/Dashboard/Opportunities/OpportunityListController.tsx
@@ -98,7 +98,7 @@ export function OpportunityListController({
 
   useEffect(() => {
     setNumOfOpps(count);
-  }, [count, setNumOfOpps]);
+  }, [count, setNumOfOpps, viewMode]);
 
   const markers = createOpportunityMarkers(opportunities);
 
